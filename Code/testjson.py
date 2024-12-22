a509445c5374fdc78dd401fe6a66c26e14fbc7dd
Noah Jackson <hello@noahjackson.com.au>
2024-12-22 13:05:12 +1000

    tested json. going to implement into main

--- Code/testjson.py
+++ Code/testjson.py
@@ -6,11 +6,7 @@ with open('config.json', 'r') as json_file:
 
 print(config_dict)
 
-config_dict["Ssid"] = "your wi-fi"
-
-print(config_dict)
-
-config_json = json.dumps(config_dict, indent=4)
+config_json = json.dumps(config_dict)
 # Write the JSON string to a file
 with open('config.json', 'w') as json_file:
     json_file.write(config_json)
